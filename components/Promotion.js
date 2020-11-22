cdbf0585f4264e40fd82904cf59ae61a7b38fa27
Ogwarf <ome504@hotmail.com>
2020-11-23 01:44:39 +0700

    update Promotion.js

---
+++ components/Promotion.js
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
+import Button from "../components/Button";
+
+export default class Promotion extends Component {
+  constructor() {
+    super();
+    this.state = {
+      numberOfLines: 3,
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.titleContainer}>
+          <Image
+            source={{ uri: this.props.data.image }}
+            style={styles.icon}
+          ></Image>
+          <TouchableOpacity
+            onPress={() => {
+              if (this.state.numberOfLines === 3) {
+                this.setState({
+                  numberOfLines: 10,
+                });
+              } else if (this.state.numberOfLines !== 3) {
+                this.setState({
+                  numberOfLines: 3,
+                });
+              }
+            }}
+            style={{ flex: 1 }}
+          >
+            <Text
+              style={styles.description}
+              numberOfLines={this.state.numberOfLines}
+            >
+              {this.props.data.description}
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.footer}>
+          <View>
+            <Text style={{ color: "#9C9797", fontSize: 13 }}>Expires</Text>
+            <Text style={{ color: "#403D56", fontWeight: "600" }}>
+              {this.props.data.exp}
+            </Text>
+          </View>
+          <View
+            style={[styles.titleContainer, { alignItems: "center" }]}
+          ></View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#F1F1F1",
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 10,
+  },
+  titleContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "flex-start",
+  },
+  icon: {
+    width: 56,
+    height: 56,
+    borderRadius: 100,
+  },
+  description: {
+    flex: 1,
+    marginLeft: 10,
+  },
+  footer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginTop: 20,
+  },
+});
--- screens/promotion.js
+++ screens/promotion.js
@@ -1,10 +1,51 @@
-import React, { Component } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { Component, useState } from "react";
+import { Text, View, StyleSheet, SafeAreaView, ScrollView } from "react-native";
+import BackPage from "../components/BackPage";
+import Promotion from "../components/Promotion";
 
 const promotion = (props) => {
+  const [promotions, setPromotions] = useState([
+    {
+      image:
+        "https://i.pinimg.com/originals/f8/8e/89/f88e898955530880794913f0efb38755.jpg",
+      description:
+        "Gift card valued at 150 or 10% off at McDonalds. Gift card valued at 150 or 10% off at McDonalds. Gift card valued at 150 or 10% off at McDonalds. Gift card valued at 150 or 10% off at McDonalds.",
+      exp: "10 - 12 - 2021",
+    },
+    {
+      image:
+        "https://i.pinimg.com/originals/f8/8e/89/f88e898955530880794913f0efb38755.jpg",
+      description:
+        "Gift card valued at 150 or 10% off at McDonalds. Gift card valued at 150 or 10% off at McDonalds.",
+      exp: "10 - 12 - 2021",
+    },
+    {
+      image:
+        "https://i.pinimg.com/originals/f8/8e/89/f88e898955530880794913f0efb38755.jpg",
+      description:
+        "Gift card valued at 150 or 10% off at McDonalds. Gift card valued at 150 or 10% off at McDonalds.",
+      exp: "10 - 12 - 2021",
+    },
+  ]);
+
   return (
     <View style={styles.screen}>
-      <Text> Promotion </Text>
+      <SafeAreaView style={styles.safeAreaView}>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          showsHorizontalScrollIndicator={false}
+          style={styles.scrollView}
+        >
+          <View style={styles.haeder}>
+            <BackPage navigation={props} path={"Restaurant"}></BackPage>
+          </View>
+          <View style={styles.content}>
+            {promotions.map((data, index) => {
+              return <Promotion data={data} key={index}></Promotion>;
+            })}
+          </View>
+        </ScrollView>
+      </SafeAreaView>
     </View>
   );
 };
@@ -12,8 +53,26 @@ const promotion = (props) => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    paddingLeft: 10,
+    paddingRight: 10,
+    backgroundColor: "#FFF",
+    display: "flex",
+    flexDirection: "column",
+  },
+  haeder: {
+    width: "100%",
+    height: 100,
+    display: "flex",
+    padding: 5,
+  },
+  circle: {
+    backgroundColor: "#F3CA1B",
+    opacity: 7,
+    borderRadius: 80,
+    position: "absolute",
+  },
+  content: {
+    marginBottom: 10,
   },
 });
 
